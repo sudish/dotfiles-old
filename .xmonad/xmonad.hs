8ad24cf3d8bccbd2b0c2e14c92165728642bec98
Sudish Joseph <sudish@gmail.com>
2011-01-29 02:23:29 -0500

    Xmonad tweaks.
    .  Work around Java bug by setting window manager name to a value in the
       jvm's harcoded list.
    .  Added a cycle windows keybinding.
    .  Use noBorders and smartBorders in layout.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -7,19 +7,22 @@
 --
 
 import XMonad
-import XMonad.Config.Gnome
-import XMonad.Hooks.DynamicLog (dzen, xmobar)
-import XMonad.Hooks.ManageDocks (avoidStruts)
-import XMonad.Util.EZConfig (additionalKeysP, checkKeymap)
-import XMonad.Actions.WindowBringer (gotoMenu, bringMenu)
 import XMonad.Actions.CycleRecentWS (cycleRecentWS)
-import XMonad.Actions.CycleWS (nextWS, prevWS,
-                                  shiftToNext, shiftToPrev, toggleWS)
-import XMonad.Prompt (defaultXPConfig)
-import XMonad.Prompt.XMonad (xmonadPrompt)
+import XMonad.Actions.CycleWindows  (cycleRecentWindows)
+import XMonad.Actions.CycleWS       (nextWS, prevWS, toggleWS,
+                                        shiftToNext, shiftToPrev)
+import XMonad.Actions.WindowBringer (gotoMenu, bringMenu)
+import XMonad.Config.Gnome          (gnomeConfig)
+import XMonad.Hooks.ManageDocks     (avoidStruts)
+import XMonad.Hooks.SetWMName       (setWMName)
+import XMonad.Layout.NoBorders      (noBorders, smartBorders)
+import XMonad.Prompt                (defaultXPConfig)
+import XMonad.Prompt.XMonad         (xmonadPrompt)
+import XMonad.Util.EZConfig         (additionalKeysP, checkKeymap)
 
 -- Layouts
-sjLayoutHook = avoidStruts $ Full ||| Tall 1 (3/100) (1/2)
+sjLayoutHook = avoidStruts $ noBorders Full
+                         ||| smartBorders (Tall 1 (3/100) (1/2))
 
 -- Keymaps
 sjModMask = mod3Mask
@@ -27,6 +30,9 @@ sjKeymap  = [ ("M3-g",          gotoMenu)
             , ("M3-S-g",        bringMenu)
               -- this next binding is on Alt-Tab, not Hyper-Tab
             , ("M1-<Tab>",      cycleRecentWS [xK_Alt_L] xK_Tab xK_grave)
+              -- on Super-Tab
+            , ("M4-<Tab>",      cycleRecentWindows
+                                            [xK_Super_L] xK_Tab xK_grave)
             , ("M3-<Right>",    nextWS)
             , ("M3-<Left>",     prevWS)
             , ("M3-S-<Right>",  shiftToNext >> nextWS)
@@ -41,7 +47,10 @@ sjConfig = gnomeConfig
          , numlockMask = mod2Mask
          , terminal = "gnome-terminal"
          , layoutHook = sjLayoutHook
-         , startupHook = return () >> checkKeymap sjConfig sjKeymap
+         , startupHook = return ()
+                         >> checkKeymap sjConfig sjKeymap
+                         -- Hack for Java 1.6 and lower
+                         >> setWMName "LG3D"
          }
          `additionalKeysP` sjKeymap
 
