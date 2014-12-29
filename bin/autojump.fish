ba6e0af6542096fab7a3242eb1e40d27f0b9498b
Sudish Joseph <sudish@gmail.com>
2014-12-28 21:44:51 -0500

    Squashed '.zsh.d/external/autojump/' changes from f75d012..c5f0473
    c5f0473 Bump to v22.2.2.
    d177aaa Merge pull request #336 from erdiaker/issue_335
    7de39a2 Merge pull request #334 from erdiaker/issue_333
    d71a354 Fixes joelthelion/autojump#335
    ec7f788 Fixes joelthelion/autojump#333
    
    git-subtree-dir: .zsh.d/external/autojump
    git-subtree-split: c5f04738374625c6933b944251ea4e1ce2ea7232

--- bin/autojump.fish
+++ bin/autojump.fish
@@ -11,12 +11,16 @@ complete -x -c j -a '(autojump --complete (commandline -t))'
 
 
 # set error file location
-if (uname) == "Darwin"
+if test (uname) = "Darwin"
     set -x AUTOJUMP_ERROR_PATH ~/Library/autojump/errors.log
 else if test -d $XDG_DATA_HOME
     set -x AUTOJUMP_ERROR_PATH $XDG_DATA_HOME/autojump/errors.log
 else
     set -x AUTOJUMP_ERROR_PATH ~/.local/share/autojump/errors.log
+end
+
+if test ! -d (dirname $AUTOJUMP_ERROR_PATH)
+    mkdir -p (dirname $AUTOJUMP_ERROR_PATH)
 end
 
 
