2917bf019ad45f361278917b8cf533734412c2c4
Sudish Joseph <sudish@gmail.com>
2019-10-12 11:23:13 -0400

    Add zsh-history-substring-search.

--- .zsh.d/external/pull-all.sh
+++ .zsh.d/external/pull-all.sh
@@ -14,10 +14,11 @@ function update_subtree {
   git subtree pull -m "Update subtree: $subtree" --squash -P $ext_dir/$subtree $remote $branch
 }
 
-update_subtree autojump                git://github.com/wting/autojump
-update_subtree git-flow-completion     git://github.com/bobthecow/git-flow-completion
-update_subtree z                       git://github.com/rupa/z.git
-update_subtree zaw                     git://github.com/zsh-users/zaw.git
-update_subtree zsh-autosuggestions     git://github.com/zsh-users/zsh-autosuggestions
-update_subtree zsh-completions         git://github.com/zsh-users/zsh-completions.git
-update_subtree zsh-syntax-highlighting git://github.com/zsh-users/zsh-syntax-highlighting.git
+update_subtree autojump                     git://github.com/wting/autojump
+update_subtree git-flow-completion          git://github.com/bobthecow/git-flow-completion
+update_subtree z                            git://github.com/rupa/z.git
+update_subtree zaw                          git://github.com/zsh-users/zaw.git
+update_subtree zsh-autosuggestions          git://github.com/zsh-users/zsh-autosuggestions
+update_subtree zsh-completions              git://github.com/zsh-users/zsh-completions.git
+update_subtree zsh-history-substring-search git://github.com/zsh-users/zsh-history-substring-search.git
+update_subtree zsh-syntax-highlighting      git://github.com/zsh-users/zsh-syntax-highlighting.git
